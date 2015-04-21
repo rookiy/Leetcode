0319efda6b873996239b34019caafaf844065ae2
rookiy <allhailtry@gmail.com>
2015-04-21 23:28:41 +0800

    Not started edition

---
+++ CountAndSay.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import sys
+
+class Solution:
+    # @param n, an integer
+    # @return a string
+    def countAndSay(self, n):
+        n_str = str(s)
+        for i in range(n):
+            tmp = ''
+            count = 0
+            for char in n_str:
+                
+            
+        
+
+def main():
+    sys.stdin = open('./1.txt', 'r')
+    solution = Solution()
+    
+    
+if __name__ == '__main__':
+    main()
