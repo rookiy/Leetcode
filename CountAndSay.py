71b5880a547a5fda1cb7d1e3e384b36196c8a2da
rookiy <allhailtry@gmail.com>
2015-04-22 15:09:26 +0800

    AC edition

--- CountAndSay.py
+++ CountAndSay.py
@@ -5,19 +5,31 @@ class Solution:
     # @param n, an integer
     # @return a string
     def countAndSay(self, n):
-        n_str = str(s)
-        for i in range(n):
-            tmp = ''
-            count = 0
-            for char in n_str:
-                
-            
+        n_list = ['1']
+        for i in range(n-1):
+            tmp = []
+            count = 1
+            char = n_list[0]
+            N = len(n_list)
+            for j in range(1,N):
+                if n_list[j] == char:
+                    count += 1
+                else:
+                    tmp.append(str(count))
+                    tmp.append(char)
+                    count = 1
+                    char = n_list[j]
+            if count != 0:
+                tmp.append(str(count))
+                tmp.append(char)
+            n_list = tmp
+        return ''.join(n_list)
         
 
 def main():
     sys.stdin = open('./1.txt', 'r')
     solution = Solution()
-    
+    print solution.countAndSay(input())
     
 if __name__ == '__main__':
     main()
