698d9287cef9bf835611c972744cabd788456ac7
rookiy <allhailtry@gmail.com>
2015-04-23 20:02:44 +0800

    Not started version (DP)

---
+++ HouseRobber.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import sys
+
+class Solution:
+    # @param {integer[]} nums
+    # @return {integer}
+    def rob(self, nums):
+        pass
+        
+        
+def main():
+    sys.stdin = open('./1.txt', 'r')
+    solution = Solution()
+    print solution.convertToTitle(input())
+    
+if __name__ == '__main__':
+    main()
